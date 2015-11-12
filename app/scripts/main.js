cd44aa4236d34758914f56b0945cb111e763f7eb
mioyn <midhun.jose485@gmail.com>
2015-11-12 22:37:46 +0530

    Created angular main application module

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,25 @@
-console.log('\'Allo \'Allo!'); // eslint-disable-line no-console
+(function() {
+    'use strict';
+    angular.module('aayiram', ['ngMessages', 'ngMaterial', 'ngAnimate', 'ui.router'])
+        .controller("DemoController", function($scope, $mdGridLayout) {
+
+            $mdGridLayout.animateWith(function gsapAnimator(grid, tiles) {
+                grid.element.css(grid.style);
+                tiles.forEach(function(t) {
+                    t.element.css(t.style);
+                });
+            });
+        })
+        .config(function($mdIconProvider) {
+            $mdIconProvider
+                .iconSet("avatar", 'http://s3-us-west-2.amazonaws.com/s.cdpn.io/181892/avatars.svg', 128);
+
+        })
+        .config(function($mdThemingProvider) {
+            $mdThemingProvider.theme('default')
+                .primaryPalette('pink')
+                .accentPalette('orange')
+                .backgroundPalette('red');
+        });
+
+}());
